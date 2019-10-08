45cd7408809e5c41ab8f5db9ba342a7047448a79
Vojtech Novak <vonovak@gmail.com>
2019-10-08 21:52:05 +0200

    remove unneeded code

--- example/android/app/src/main/java/com/example/MainApplication.java
+++ example/android/app/src/main/java/com/example/MainApplication.java
@@ -7,7 +7,6 @@ import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.reactnativecommunity.slider.ReactSliderPackage;
 
 import java.util.List;
 
--- react-native.config.js
+++ react-native.config.js
@@ -2,6 +2,8 @@ const root = process.cwd();
 
 module.exports = {
   dependencies: {
+    // the key here does not matter as long as it's not referenced from native files
+    // CLI will use root to find podspec and android module.
     'react-native-slider': {
       root,
     },
