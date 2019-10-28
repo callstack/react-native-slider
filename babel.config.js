8d13010fbceed11afb86e16ddf995d4ad7b2b3b5
Wojciech Szafraniec <feelun132@gmail.com>
2019-10-28 12:41:48 +0100

    WIP: Workspace setup

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,14 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    [
-      'module-resolver',
-      {
-        alias: {
-          '@react-native-community/slider': './js/Slider.js',
-        },
-        cwd: 'babelrc',
-      },
-    ],
-  ],
+  // plugins: [
+  //   [
+  //     'module-resolver',
+  //     {
+  //       alias: {
+  //         '@react-native-community/slider': './src/js/Slider.js',
+  //       },
+  //       cwd: 'babelrc',
+  //     },
+  //   ],
+  // ],
 };
