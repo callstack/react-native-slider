419b4a5764abd79b3f09195c3d5c58ac1fe85bff
ShaMan123 <shacharnen@gmail.com>
2020-03-29 17:36:08 +0300

    Update metro.config.js
    the added config enables working on window/android, building native code without metro crashing all the time

--- metro.config.js
+++ metro.config.js
@@ -5,7 +5,18 @@
  * @format
  */
 
+const blacklist = require('metro-config/src/defaults/blacklist');
+const path = require('path');
+
 module.exports = {
+  resolver: {
+    blacklistRE: blacklist([
+      path.resolve(__dirname, 'android'),
+      path.resolve(__dirname, 'ios'),
+      path.resolve(__dirname, 'example', 'android'),
+      path.resolve(__dirname, 'example', 'ios'),
+    ])
+  },
   transformer: {
     getTransformOptions: async () => ({
       transform: {
