9a5792dbfb18751b8b158781142bd3d41c625ed6
François Billioud <darksharcoux@hotmail.com>
2020-05-19 22:01:16 +0200

    Fix #163 for Web

--- src/js/RNCSliderNativeComponent.web.js
+++ src/js/RNCSliderNativeComponent.web.js
@@ -223,12 +223,27 @@ const RCTSliderWebComponent = React.forwardRef(
       thumbStyle,
     );
 
+    // We should round number with the same precision as the min, max or step values if provided
+    function calculatePrecision () {
+      if (!step) return Infinity
+      else {
+        // Calculate the number of decimals we can encounter in the results
+        const decimals = [minimumValue, maximumValue, step].map(value => ((value + '').split('.').pop() || '').length)
+        return Math.max(...decimals)
+      }
+    }
+    const decimalPrecision = React.useRef(calculatePrecision())
+    React.useEffect(() => { decimalPrecision.current = calculatePrecision() }, [step])
+  
     const updateValue = newValue => {
+      // Ensure that the value is correctly rounded
+      const hardRounded = decimalPrecision.current < 20 ? Number.parseFloat(newValue.toFixed(decimalPrecision.current)) : newValue
+
       // Ensure that the new value is still between the bounds
       const withinBounds = Math.max(
         minimumValue,
-        Math.min(newValue, maximumValue),
-      );
+        Math.min(hardRounded, maximumValue)
+      )
       if (value !== withinBounds) {
         setValue(withinBounds);
         onValueChange(withinBounds);
