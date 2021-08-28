275d9a68c8fefed2869b8ca3aecd3b7474193414
Bartosz Klonowski <70535775+BartoszKlonowski@users.noreply.github.com>
2021-08-28 02:08:59 +0200

    Recreate the Windows Example app project

--- example/windows/example/ReactPackageProvider.cpp
+++ example/windows/example/ReactPackageProvider.cpp
@@ -2,10 +2,9 @@
 #include "ReactPackageProvider.h"
 #include "NativeModules.h"
 
-
 using namespace winrt::Microsoft::ReactNative;
 
-namespace winrt::SliderTestWindows::implementation
+namespace winrt::example::implementation
 {
 
 void ReactPackageProvider::CreatePackage(IReactPackageBuilder const &packageBuilder) noexcept
@@ -13,6 +12,4 @@ void ReactPackageProvider::CreatePackage(IReactPackageBuilder const &packageBuil
     AddAttributedModules(packageBuilder);
 }
 
-} // namespace winrt::SliderTestWindows::implementation
-
-
+} // namespace winrt::example::implementation
--- example/windows/example/ReactPackageProvider.h
+++ example/windows/example/ReactPackageProvider.h
@@ -2,14 +2,12 @@
 
 #include "winrt/Microsoft.ReactNative.h"
 
-
-namespace winrt::SliderTestWindows::implementation
+namespace winrt::example::implementation
 {
     struct ReactPackageProvider : winrt::implements<ReactPackageProvider, winrt::Microsoft::ReactNative::IReactPackageProvider>
     {
     public: // IReactPackageProvider
         void CreatePackage(winrt::Microsoft::ReactNative::IReactPackageBuilder const &packageBuilder) noexcept;
     };
-} // namespace winrt::SliderTestWindows::implementation
-
+} // namespace winrt::example::implementation
 
