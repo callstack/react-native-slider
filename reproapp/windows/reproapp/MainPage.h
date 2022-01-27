630f7263dbc4da1db35e437f894242601cfb2507
Bartosz Klonowski <70535775+BartoszKlonowski@users.noreply.github.com>
2022-01-27 21:04:39 +0100

    Add Windows project to repro app

---
+++ reproapp/windows/reproapp/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::reproapp::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::reproapp::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
