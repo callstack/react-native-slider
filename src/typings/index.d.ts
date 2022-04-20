68a9de10eaa2cca51d4621d4c18a795da87166fd
Bartosz Klonowski <70535775+BartoszKlonowski@users.noreply.github.com>
2022-04-20 18:57:52 +0200

    Fix: Type error when using Slider with props as ref (#369)
    * Introduce SliderReferenceType
    
    * Use SliderReferenceType for ref type

--- src/typings/index.d.ts
+++ src/typings/index.d.ts
@@ -1,6 +1,11 @@
 import * as React from "react";
 import * as ReactNative from "react-native";
 
+type SliderReferenceType = (
+  React.MutableRefObject<SliderRef> &
+  React.LegacyRef<Slider>
+) | undefined;
+
 export interface SliderPropsAndroid extends ReactNative.ViewProps {
   /**
    * Color of the foreground switch grip.
@@ -145,7 +150,7 @@ export interface SliderProps extends SliderPropsIOS, SliderPropsAndroid, SliderP
   /**
    * Reference object.
    */
-  ref?: React.MutableRefObject<SliderRef>;
+  ref?: SliderReferenceType;
 }
 
 /**
