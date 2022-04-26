4df38947d15e5607620c9c01f027686727b444b2
Bartosz Klonowski <70535775+BartoszKlonowski@users.noreply.github.com>
2022-04-27 00:51:13 +0200

    Remove Facebook licence header

--- src/js/RNCSliderNativeComponent.js
+++ src/js/RNCSliderNativeComponent.js
@@ -1,15 +1,3 @@
-/**
- * Copyright (c) Facebook, Inc. and its affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- *
- * @format
- * @flow
- */
-
-'use strict';
-
 import {requireNativeComponent} from 'react-native';
 
 import type {ColorValue} from 'react-native/Libraries/StyleSheet/StyleSheetTypes';
--- src/js/RNCSliderNativeComponent.web.js
+++ src/js/RNCSliderNativeComponent.web.js
@@ -1,11 +1,3 @@
-/**
- * Copyright (c) Facebook, Inc. and its affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- *
- */
-
 import ReactDOM from 'react-dom';
 import React, {useCallback} from 'react';
 import {View, StyleSheet} from 'react-native';
