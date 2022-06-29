e463d9a4f1c85e22ccac3eb1709eb630195ccd1e
Bartosz Klonowski <70535775+BartoszKlonowski@users.noreply.github.com>
2022-06-30 01:16:56 +0200

    Update docs regarding controlled value (#392)
    * Update value description in Slider component
    
    * Update controlled value description in types
    
    * Correct typos in code docs
    
    * Correct typos in README files

--- src/js/Slider.js
+++ src/js/Slider.js
@@ -77,11 +77,14 @@ type Props = $ReadOnly<{|
   style?: ?ViewStyleProp,
 
   /**
-   * Initial value of the slider. The value should be between minimumValue
-   * and maximumValue, which default to 0 and 1 respectively.
+   * Write-only property representing the value of the slider.
+   * Can be used to programmatically control the position of the thumb.
+   * Entered once at the beginning still acts as an initial value.
+   * The value should be between minimumValue and maximumValue,
+   * which default to 0 and 1 respectively.
    * Default value is 0.
    *
-   * *This is not a controlled component*, you don't need to update the
+   * This is not a controlled component, you don't need to update the
    * value during dragging.
    */
   value?: ?number,
