a9ae1112b0b857fab6fa49ee6487269565617628
BartoszKlonowski <Bartosz.Klonowski@callstack.com>
2022-07-09 16:20:25 +0200

    Recreate the example project with react-native from scratch

--- example/App.js
+++ example/App.js
@@ -1,52 +1,112 @@
-import React, {Component} from 'react';
-import {StyleSheet, Text, ScrollView, View, Platform} from 'react-native';
-import {examples} from './SliderExample';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
+import React from 'react';
+import type {Node} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const Section = ({children, title}): Node => {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+};
+
+const App: () => Node = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <ScrollView
-        style={styles.scrollView}
-        contentContainerStyle={styles.container}>
-        <Text testID="testTextId" style={styles.title}>
-          {'<Slider />'}
-        </Text>
-        {examples
-          .filter(e => !e.platform || e.platform === Platform.OS)
-          .map((e, i) => (
-            <View key={`slider${i}`} style={styles.sliderWidget}>
-              <Text style={styles.instructions}>{e.title}</Text>
-              {e.render()}
-            </View>
-          ))}
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Header />
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <Section title="Step One">
+            Edit <Text style={styles.highlight}>App.js</Text> to change this
+            screen and then come back to see your edits.
+          </Section>
+          <Section title="See Your Changes">
+            <ReloadInstructions />
+          </Section>
+          <Section title="Debug">
+            <DebugInstructions />
+          </Section>
+          <Section title="Learn More">
+            Read the docs to discover what to do next:
+          </Section>
+          <LearnMoreLinks />
+        </View>
       </ScrollView>
-    );
-  }
-}
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: '#F5FCFF',
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
   },
-  container: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingVertical: 20,
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
   },
-  title: {
-    fontSize: 20,
-    textAlign: 'center',
-    width: '100%',
-    marginVertical: 20,
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-    fontSize: 20,
-  },
-  sliderWidget: {
-    marginVertical: 30,
+  highlight: {
+    fontWeight: '700',
   },
 });
+
+export default App;
