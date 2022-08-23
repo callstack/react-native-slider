56fa6153df1d45391f230a614f87a723fdc280d2
BartoszKlonowski <Bartosz.Klonowski@callstack.com>
2022-08-24 01:26:01 +0200

    Configure TS for package compilation with Babel

--- package/babel.config.js
+++ package/babel.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  presets: ['@babel/preset-flow', 'module:metro-react-native-babel-preset'],
+  presets: ['@babel/preset-typescript', 'module:metro-react-native-babel-preset'],
 };
