115ac096fb1ec06644b21c815e50165d28b7b4fd
BartoszKlonowski <Bartosz.Klonowski@callstack.com>
2024-09-10 09:50:45 +0200

    Apply the fix for calculating the step resolution

--- package/src/utils/constants.ts
+++ package/src/utils/constants.ts
@@ -1,8 +1,10 @@
+import {Platform} from 'react-native';
+
 export const constants = {
   MARGIN_HORIZONTAL_PADDING: 0.05,
   STEP_NUMBER_TEXT_FONT_SMALL: 8,
   STEP_NUMBER_TEXT_FONT_BIG: 12,
   LIMIT_MIN_VALUE: Number.MIN_SAFE_INTEGER,
   LIMIT_MAX_VALUE: Number.MAX_SAFE_INTEGER,
-  DEFAULT_STEP_RESOLUTION: 100,
+  DEFAULT_STEP_RESOLUTION: Platform.OS === 'android' ? 128 : 1000,
 };
