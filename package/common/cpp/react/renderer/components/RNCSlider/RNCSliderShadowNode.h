acb44e55eb92da84db9dfabecb8510242b524eaa
maciej.lodygowski <maciej.lodygowski@callstack.com>
2024-09-12 13:16:56 +0200

    fix: remove invalid import for static libraries

--- package/common/cpp/react/renderer/components/RNCSlider/RNCSliderShadowNode.h
+++ package/common/cpp/react/renderer/components/RNCSlider/RNCSliderShadowNode.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <jsi/jsi.h>
-#include <react/renderer/components/rncore/EventEmitters.h>
 #include <react/renderer/components/RNCSlider/RNCSliderState.h>
 #include <react/renderer/components/RNCSlider/Props.h>
 #include <react/renderer/components/RNCSlider/EventEmitters.h>
