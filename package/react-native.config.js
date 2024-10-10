fd25fd568bafd6fa5bd5a70d58df5e23410c6464
chrfalch <christian.falch@gmail.com>
2024-10-10 16:28:08 +0200

    Codereview: Removed test for code gen alltogether

--- package/react-native.config.js
+++ package/react-native.config.js
@@ -1,24 +1,11 @@
-let supportsCodegenConfig = false;
-try {
-  const rnCliAndroidVersion = require(
-    '@react-native-community/cli-platform-android/package.json',
-  ).version;
-  const [major] = rnCliAndroidVersion.split('.');
-  supportsCodegenConfig = major >= 9;
-} catch (e) {
-  // ignore
-}
-
 module.exports = {
   dependency: {
     platforms: {
-      android: supportsCodegenConfig
-        ? {
-            libraryName: 'RNCSlider',
-            componentDescriptors: ["RNCSliderComponentDescriptor"],
-            cmakeListsPath: 'src/main/jni/CMakeLists.txt',
-          }
-        : {},
+      android: {
+        libraryName: 'RNCSlider',
+        componentDescriptors: ['RNCSliderComponentDescriptor'],
+        cmakeListsPath: 'src/main/jni/CMakeLists.txt',
+      },
     },
   },
 };
