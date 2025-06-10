77f4d40ba829c806b093273136c0edabe728341f
Wiktor Jaszczuk <wiktor.jaszczuk@callstack.com>
2025-06-10 14:30:24 +0200

    Refactor tests to use @testing-library/react-native

--- package/src/__tests__/Slider.test.tsx
+++ package/src/__tests__/Slider.test.tsx
@@ -1,3 +1,4 @@
+import {render, screen} from '@testing-library/react-native';
 import * as React from 'react';
 import renderer from 'react-test-renderer';
 import Slider from '../Slider';
@@ -5,84 +6,77 @@ import {View} from 'react-native';
 
 describe('<Slider />', () => {
   it('renders enabled slider', () => {
-    const tree = renderer.create(<Slider />).toJSON();
+    render(<Slider />);
 
-    expect(tree).toMatchSnapshot();
+    expect(screen).toMatchSnapshot();
   });
 
   it('renders disabled slider', () => {
-    const tree = renderer.create(<Slider disabled />).toJSON();
+    render(<Slider disabled />);
 
-    expect(tree).toMatchSnapshot();
+    expect(screen).toMatchSnapshot();
   });
 
   it('accessibilityState disabled sets disabled={true}', () => {
-    const tree = renderer
-      .create(<Slider accessibilityState={{disabled: true}} />)
-      .toJSON();
+    render(<Slider accessibilityState={{disabled: true}} />);
 
-    expect(tree).toMatchSnapshot();
+    expect(screen).toMatchSnapshot();
   });
 
   it('disabled prop overrides accessibilityState.disabled', () => {
-    const tree = renderer
-      .create(<Slider disabled accessibilityState={{disabled: false}} />)
-      .toJSON();
+    render(<Slider disabled accessibilityState={{disabled: false}} />);
 
-    expect(tree).toMatchSnapshot();
+    expect(screen).toMatchSnapshot();
   });
 
   it('disabled prop overrides accessibilityState.enabled', () => {
-    const tree = renderer
-      .create(<Slider disabled={false} accessibilityState={{disabled: true}} />)
-      .toJSON();
+    render(<Slider disabled={false} accessibilityState={{disabled: true}} />);
 
-    expect(tree).toMatchSnapshot();
+    expect(screen).toMatchSnapshot();
   });
 
   it('renders a slider with custom props', () => {
-    const tree = renderer
-      .create(
-        <Slider
-          value={0.5}
-          minimumValue={-1}
-          maximumValue={2}
-          step={0.25}
-          minimumTrackTintColor={'blue'}
-          maximumTrackTintColor={'red'}
-          thumbTintColor={'green'}
-          onSlidingComplete={() => {}}
-          onValueChange={() => {}}
-          lowerLimit={0}
-          upperLimit={1}
-        />,
-      )
-      .toJSON();
+    render(
+      <Slider
+        value={0.5}
+        minimumValue={-1}
+        maximumValue={2}
+        step={0.25}
+        minimumTrackTintColor={'blue'}
+        maximumTrackTintColor={'red'}
+        thumbTintColor={'green'}
+        onSlidingComplete={() => {}}
+        onValueChange={() => {}}
+        lowerLimit={0}
+        upperLimit={1}
+      />,
+    );
 
-    expect(tree).toMatchSnapshot();
+    expect(screen).toMatchSnapshot();
   });
 
   it('renders a slider with custom stepMaker', () => {
-    const tree = renderer
-      .create(
-        <Slider
-          value={2}
-          minimumValue={0}
-          maximumValue={4}
-          StepMarker={({stepMarked}) => {
-            return stepMarked ? (
-              <View>
-                <View style={{width: 10, backgroundColor: 'red'}} />
-              </View>
-            ) : (
-              <View>
-                <View style={{width: 10, backgroundColor: 'green'}} />
-              </View>
-            );
-          }}
-        />,
-      )
-      .toJSON();
+    render(
+      <Slider
+        value={2}
+        minimumValue={0}
+        maximumValue={4}
+        StepMarker={({stepMarked}) => {
+          return stepMarked ? (
+            <View>
+              <View style={{width: 10, backgroundColor: 'red'}} />
+            </View>
+          ) : (
+            <View>
+              <View style={{width: 10, backgroundColor: 'green'}} />
+            </View>
+          );
+        }}
+      />,
+    );
+
+    expect(screen).toMatchSnapshot();
+  });
 
     expect(tree).toMatchSnapshot();
   });
