0360d6eb03f7fa7c111b168e15b6aa580090accf
BartoszKlonowski <Bartosz.Klonowski@callstack.com>
2025-10-24 16:54:21 +0200

    Import CK background and use it for the example app

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,119 +1,37 @@
 import React, {useState} from 'react';
 import {
+  Image,
+  ImageBackground,
   Platform,
   SafeAreaView,
-  ScrollView,
+  StatusBar,
   StyleSheet,
   Text,
-  View,
 } from 'react-native';
-import {examples, Props as ExamplesTabProperties} from './Examples';
-import {propsExamples, Props as PropsTabProperties} from './Props';
-import PagerView from 'react-native-pager-view';
-import Slider from '@react-native-community/slider';
 
 const App = () => {
-  const [currentPage, setCurrentPage] = useState(0);
-  const titles = ['Examples', 'Props'];
-
-  const renderExampleTab = (
-    sliders: PropsTabProperties[] | ExamplesTabProperties[],
-    filtered?: boolean,
-  ) => {
-    return (
-      <View>
-        <ScrollView
-          style={styles.scrollView}
-          contentContainerStyle={styles.container}>
-          {(filtered
-            ? (sliders as ExamplesTabProperties[]).filter(
-                e => !e.platform || e.platform === Platform.OS,
-              )
-            : sliders
-          ).map((e, i) => (
-            <View key={`slider${i}`} style={styles.sliderWidget}>
-              <Text style={styles.instructions}>{e.title}</Text>
-              {e.render()}
-            </View>
-          ))}
-        </ScrollView>
-      </View>
-    );
-  };
+  const backgroundImage = Image.resolveAssetSource(require('./resources/Light.png'));
 
   return (
-    <SafeAreaView style={styles.homeScreenContainer}>
-      <View>
-        <Slider
-          step={1}
-          maximumValue={3}
-          minimumValue={0}
-          style={styles.slider}
-          value={currentPage + 1}
-          thumbTintColor={pageViewPositionSlider.thumbColor}
-          disabled
-          maximumTrackTintColor={pageViewPositionSlider.trackColor}
-          minimumTrackTintColor={pageViewPositionSlider.trackColor}
-        />
-        <Text testID="testTextId" style={styles.title}>
-          {titles[currentPage]}
-        </Text>
-      </View>
-      <PagerView
-        initialPage={0}
-        style={styles.pagerViewContainer}
-        onPageSelected={e => {
-          setCurrentPage(e.nativeEvent.position);
-        }}>
-        {renderExampleTab(examples, true)}
-        {renderExampleTab(propsExamples)}
-      </PagerView>
-    </SafeAreaView>
+    <ImageBackground source={backgroundImage} style={styles.mainContainer}>
+      <SafeAreaView style={styles.safeArea}>
+        <Text style={styles.introText}>Slider can be used in various scenarios...</Text>
+      </SafeAreaView>
+    </ImageBackground>
   );
 };
 
 export default App;
 
-const pageViewPositionSlider = {
-  trackColor: '#ABABAB',
-  thumbColor: '#1411AB',
-  style: {
-    width: '100%',
-  },
-};
-
 const styles = StyleSheet.create({
-  slider: {
-    width: '100%',
-  },
-  pagerViewContainer: {
+  mainContainer: {
     flex: 1,
   },
-  homeScreenContainer: {
-    flex: 1,
+  safeArea: {
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
   },
-  scrollView: {
-    backgroundColor: '#F5FCFF',
-  },
-  container: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingVertical: 20,
-  },
-  title: {
-    fontSize: 30,
-    color: pageViewPositionSlider.thumbColor,
-    textAlign: 'center',
-    width: '100%',
-    marginVertical: 20,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-    fontSize: 20,
-  },
-  sliderWidget: {
-    marginVertical: 30,
+  introText: {
+    textAlign: "center",
+    fontFamily: "Allience No.2"
   },
 });
