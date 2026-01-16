f0f6b70ade9ad439c15db8f0e314321793e25261
BartoszKlonowski <Bartosz.Klonowski@callstack.com>
2026-01-16 11:00:18 +0100

    Introduce __test__ directory and implement first basic test
    This commit adds the separate __test__ directory which will keep all tests
    of the whole library.
    This is to make sure, that when later publishing sources to npm, tests won't be
    part of the deliveries.
    
    The first test added or the StepNumber component is the most basic, because:
    * this component is rather simple (for now)
    * this is to make sure, in this commit, that tests work without hiccups.

---
+++ package/__test__/components/StepNumber.test.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { render } from "@testing-library/react-native";
+import { StepNumber } from "../../src/components/StepNumber"
+
+describe("StepNumber", () => {
+  it("Displays number of step according to given index", () => {
+    const {getByText} = render(<StepNumber i={0} style={undefined} />);
+    expect(getByText("0")).toBeDefined();
+  });
+});
