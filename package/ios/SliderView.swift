8bcef82b01568439a465a8e4ee8f497a3eb566f4
BartoszKlonowski <Bartosz.Klonowski@callstack.com>
2026-07-24 14:49:16 +0200

    Add SliderView objc class to SliderView

--- package/ios/SliderView.swift
+++ package/ios/SliderView.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import UIKit
 
 struct SliderComponent: View {
   @EnvironmentObject var state: SliderState
@@ -39,5 +40,52 @@ class SliderView: UIView {
 
   override func layoutSubviews() {
     subviews.first?.frame = self.frame
+  }
+}
+
+@objc(SliderView)
+public class SliderView: UIView {
+  private let state = SliderState()
+  private var hostingController: UIHostingController<SliderComponent>?
+
+  @objc public var minValue: Double = 0 {
+    didSet {
+      state.setMinValue(minValue)
+    }
+  }
+
+  @objc public var maxValue: Double = 1 {
+    didSet {
+      state.setMaxValue(maxValue)
+    }
+  }
+
+  @objc public var step: Double = 0 {
+    didSet {
+      state.setStep(step)
+    }
+  }
+
+  public override init(frame: CGRect) {
+    super.init(frame: frame)
+
+    clipsToBounds = false
+    layer.masksToBounds = false
+
+    let hostingController = UIHostingController(rootView: SliderComponent(state: state))
+    hostingController.view.backgroundColor = .clear
+    hostingController.view.clipsToBounds = false
+    hostingController.view.layer.masksToBounds = false
+    addSubview(hostingController.view)
+    self.hostingController = hostingController
+  }
+
+  public required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  public override func layoutSubviews() {
+    super.layoutSubviews()
+    hostingController?.view.frame = bounds
   }
 }
